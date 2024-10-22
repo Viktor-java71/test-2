bcf827bbaffa1d6632301d597d6685842e9c1a4d
Viktor-java71 <viktorkasanzew@gmail.com>
2024-10-23 01:20:38 +0200

    commit main

--- src/ListHandler.java
+++ src/ListHandler.java
@@ -7,7 +7,7 @@ public class ListHandler {
         this.personList = personList;
     }
 
-
+    // Метод проверяет, есть ли человек с заданным возрастом в списке
     public    boolean isPersonByAgeExists( int age){
         for (Person person : personList){
             if (person.getAge() == age ){
--- src/Main.java
+++ src/Main.java
@@ -5,71 +5,46 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Integer i1 = 10;  // Integer i1= new Integer(10)
-        Integer i2 = 10;  // Integer i1= new Integer(10)
-        Double d = 5.3;  // Double d = new Double(5.3)
-        double q;
+        // Создание переменных типов-оберток (wrapper classes) для примитивов
+        Integer i1 = 10;  // Эквивалентно: Integer i1 = new Integer(10), создается объект типа Integer со значением 10
+        Integer i2 = 10;  // Эквивалентно: Integer i2 = new Integer(10), создается еще один объект типа Integer со значением 10
+        Double d = 5.3;   // Эквивалентно: Double d = new Double(5.3), создается объект типа Double со значением 5.3
+        double q;         // Объявляется переменная примитивного типа double, но значение не присваивается
 
+        // Автоматическая распаковка (auto-unboxing)
+        int k = i1;  // Автоматическая распаковка объекта i1 в примитив int, эквивалентно i1.intValue()
 
-        int k = i1; //i1.intValue();    auto unboxing
-        i1=i1+i2;
+        // Сложение двух объектов Integer с автоматической распаковкой и упаковкой
+        i1 = i1 + i2;  // Операция распаковки (i1.intValue() + i2.intValue()) и затем упаковка результата обратно в объект Integer
 
+        // Вызов метода для ввода списка целых чисел с клавиатуры
         List<Integer> ints = getIntsFromScanner();
-        System.out.println(ints);
-
+        System.out.println(ints);  // Печать полученного списка на экран
     }
 
+    // Метод для получения списка целых чисел от пользователя через сканнер
     public static List<Integer> getIntsFromScanner(){
-
+        // Создаем сканнер для чтения данных с консоли
         Scanner scanner = new Scanner(System.in);
+
+        // Создаем список для хранения целых чисел, введенных пользователем
         List<Integer> result = new ArrayList<>();
 
+        // Бесконечный цикл для запроса чисел у пользователя
         while (true){
-            System.out.println("Введите цклое число или 0 для завершения  ");
+            // Печатаем приглашение для ввода целого числа
+            System.out.println("Введите целое число или 0 для завершения");
+
+            // Читаем число, введенное пользователем
             int number = scanner.nextInt();
-            if (number==0){
+
+            // Если введен 0, то возвращаем текущий список и завершаем цикл
+            if (number == 0) {
                 return result;
             }
+
+            // Добавляем введенное число в список
             result.add(number);
         }
-
     }
-    /*
-        public static boolean isExists(List<Integer> list, int number){
-            for( Integer element : list    ){
-                if (element.equals(number)) {
-                    return true;
-                }
-            }
-            return false;
-        }
-    */
-    public  static  boolean isPersonByAgeExists(List<Person> list, int age){
-        for (Person person : list){
-            if (person.getAge() == age ){
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    public static boolean isExists(List<Integer> list, int number){
-        return list.contains(number);
-    }
-
-    public static int indexOf(List<Integer> list, int number){
-        return  list.indexOf(number);
-    }
-
-
-    public static boolean isPersonExist(List<Person> list, Person person){   // create equals() in Person !!!!
-        return list.contains(person);
-    }
-
-
-
-
-
-
 }
